df4006ba61a09e489e6e06c2d147956ccc3cb895
isaiahshiner <shiner.isaiah@gmail.com>
2015-01-04 12:40:31 -0500

    Added "HelloWorld2"
    Added all the files phonegap originally generated plus the stuff Andrew
    and Tyler wrote on the 1/3/15

---
+++ www/initialize.js
@@ -0,0 +1,3 @@
+//alert("Initialize.js has been called");
+Parse.initialize("1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw", "nNWuXfGI2ujbWBzxd5Om3F3OgFIIZRfvonOxfdEc");
+//alert("Successfully initialized Parse");
