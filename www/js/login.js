ecad23331a2885073a9c6e8698d00244df4e94da
Andrew Dalzon <andrewdalzon@gmail.com>
2015-01-06 19:47:14 -0500

    Parse + js = frustration
    -Reverted my changes to the logIn and signUp functions to what they were
    when Tyler said it worked, and now I get a 'undefined unauthorized'
    error when I try to log in on the app. Sending it to Tyler for
    troubleshooting.

--- www/js/login.js
+++ www/js/login.js
@@ -1,16 +1,15 @@
 function login() {
-	alert("Entered Login Function");
+
 	var user = document.getElementById("Username").value;
 	var pass = document.getElementById("Password").value;
-	alert("Username is " + user);
-	alert("Password is " + pass);
+
 	Parse.User.logIn(user, pass, {
 		success : function(user) {
 			alert("Entered success");
 			window.location = "home.html";
 		},
 		error : function(user, error) {
-			alert("Your username and password do not match!");
+			alert("Error: " + error.code + " " + error.message);
 		}
 	});
 
