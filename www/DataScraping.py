f1252eb401a6114d727dbb8f0567cb68cc5d0967
Laucksy <phisigmapi314@gmail.com>
2015-02-05 14:30:31 -0500

    Efficient Data Scraping
    The Talon scraping was made more efficient. Now it gets the links for
    each year, then loops through each year grabbing the links for each
    issue from the 2002-2003 year to the 2013-2014 year.

--- www/DataScraping.py
+++ www/DataScraping.py
@@ -5,6 +5,7 @@ originalHTML = urllib.urlopen("http://www.clsd.k12.pa.us/CCHigh.cfm?subpage=4314
 sourceCode = originalHTML.read()
 soup = BeautifulSoup(sourceCode)
 listOfStrings = ""
+numberOfLinks = 0
 
 for link in soup.find_all("a"):
     listOfStrings = listOfStrings + link.text + link.get("href")
@@ -13,33 +14,30 @@ myList = listOfStrings.split("2002-2003")
 finalList = myList[1].split("School Information")
 yearLinks = []
 index = 0
+listString = finalList[0]
 
-while finalList.hasNext():
-    yearLinks[index] = "http://www.clsd.k12.pa.us/" + (finalList[0].split("200" +str(index + 3) + "-200" + str(index + 4)))[0]
+while len(listString) > 45:
+    tempString = listString.split(str(2000 + index + 3) + "-" + str(2000 + index + 4))[0]
+    if index != 0:
+        yearLinks.extend([("http://www.clsd.k12.pa.us/" + tempString[9:])])
+    else:
+        yearLinks.extend([("http://www.clsd.k12.pa.us/" + tempString)])
+    listString = listString.split(tempString)[1]
     print yearLinks[index]
     index = index + 1
-
-'''
-firstLink = "http://www.clsd.k12.pa.us/" + (finalList[0].split("2003-2004"))[0]
-print firstLink
-secondLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2004-2005"))[1]).split("2005-2006"))[0]
-print secondLink
-thirdLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2005-2006"))[1]).split("2006-2007"))[0]
-print thirdLink
-fourthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2006-2007"))[1]).split("2007-2008"))[0]
-print fourthLink
-fifthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2007-2008"))[1]).split("2008-2009"))[0]
-print fifthLink
-sixthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2008-2009"))[1]).split("2009-2010"))[0]
-print sixthLink
-seventhLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2009-2010"))[1]).split("2010-2011"))[0]
-print seventhLink
-eighthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2010-2011"))[1]).split("2011-2012"))[0]
-print eighthLink
-ninthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2011-2012"))[1]).split("2012-2013"))[0]
-print ninthLink
-tenthLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2012-2013"))[1]).split("2013-2014"))[0]
-print tenthLink
-eleventhLink = "http://www.clsd.k12.pa.us/" + (((finalList[0].split("2013-2014"))[1]).split("2014-2015"))[0]
-print eleventhLink
-'''
+    
+articleLinks = []     
+for i in range(0,len(yearLinks)):
+    print "\n\n\n\n\n" + str(2000 + i + 2) + "-" + str(2000 + i + 3)
+    issueLinks = []
+    currentLink = yearLinks[i]
+    html = urllib.urlopen(currentLink)
+    source = html.read()
+    secondSoup = BeautifulSoup(source)
+    moreLinks = ""
+    for link in secondSoup.find_all("a"):
+        if "Issue" in link.text:
+            numberOfLinks += 1
+            issueLinks.extend(link.get("href"))
+            print(link.get("href"))
+    articleLinks.extend(issueLinks)
