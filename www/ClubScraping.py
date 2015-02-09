d8798a9e32e65ebef4f2838c733b062733e006ac
Laucksy <phisigmapi314@gmail.com>
2015-02-09 14:29:32 -0500

    Even More Data Scraping
    Crying at trying to connect to Parse. RIP In Peach Byte Club.

--- www/ClubScraping.py
+++ www/ClubScraping.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib
-import execjs
 
-originalHTML = urllib.urlopen("http://www.clsd.k12.pa.us/CCHigh.cfm?subpage=43147")
+#Variables and stuff
+originalHTML = urllib.urlopen("http://www.clsd.k12.pa.us/CCHigh.cfm?subpage=41913")
 sourceCode = originalHTML.read()
 soup = BeautifulSoup(sourceCode)
 listOfStrings = []
@@ -10,21 +10,26 @@ years = []
 yearLinks = []
 index = 0
 
+#Navigates through publications source code / "parse tree" to find where the links are being held hostage
 listOfStrings =soup.find("ul", id="qlItems").find("ul", {"class": "deeper"}).find("ul", {"class": "deeper"}).find("ul", {"class": "deeper"}).findAll("li", recursive = False)
 
+#Grabs herefs and the link they correspond too
 for index in range(0, len(listOfStrings)):
     years.append(listOfStrings[index].a.string)
     listOfStrings[index] = listOfStrings[index].a["href"]
     print listOfStrings[index]
     print years[index]
 
+#index done equals cero
 index = 0
 
+#adds stuffs to the thinger that the website doesn't already have done included
 while index < len(listOfStrings):
     yearLinks.append("http://www.clsd.k12.pa.us/" + listOfStrings[index])
     print yearLinks[index]
     index += 1
     
+#finds each year's links and try to store them prettily like the souop we has that was beautiful like soup
 numberOfLinks = 0
 articleLinks = []     
 for i in range(0,len(yearLinks)):
@@ -41,18 +46,3 @@ for i in range(0,len(yearLinks)):
             issueLinks.append("http://www.clsd.k12.pa.us" + link.get("href"))
             print(link.get("href"))
     articleLinks.append(issueLinks)
-
-js = execjs.compile("""
-     function upload(articleLinks) {
-         return articleLinks;
-     }
-""")
-print js.call("upload", articleLinks)
-    
-    
-    
-    
-    
-    
-    
-    
