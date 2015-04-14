bb45b5656227b1c525f773d3aedd29de47b569dd
ljcrafty <ljcrafty@comcast.net>
2015-04-14 13:23:45 -0400

    Lots of Pretty stuff
    Changed a lot of backgrounds and made a new menu etc.

---
+++ www/LunchScraping.py
@@ -0,0 +1,3 @@
+#Lauren Johnston
+#scrapes for the lunch calendar and puts the info on parse
+
--- www/TalonScraping.py
+++ www/TalonScraping.py
@@ -1,3 +1,5 @@
+#Erik
+#scrapes for current talon articles and uploads their info to parse
 from bs4 import BeautifulSoup
 import urllib
 import json,httplib
