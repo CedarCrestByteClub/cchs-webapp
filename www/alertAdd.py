8879824fc1e2d6231c2edde4fbb2dc63a8332e43
ljcrafty <ljcrafty@comcast.net>
2015-04-24 13:01:47 -0400

    Animate!
    I added the notification thing to hold whether or not there are closings
    etc. and added an animation that "rings" a bell when there is a new
    notification.

---
+++ www/alertAdd.py
@@ -0,0 +1,48 @@
+#Lauren Johnston
+#test file to add alerts
+
+import urllib
+import json,httplib
+
+#HTTP Request to grab stuff from database using REST API
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
+idNumbers = []
+params = urllib.urlencode({"limit":1000})
+'''
+#Sends request and returns results in a list
+connection.request('GET', '/1/classes/SchoolAlert?%s' % params, '', {
+    "X-Parse-Application-Id": "1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw",
+    "X-Parse-REST-API-Key": "wL9fgRcbDT7UE6slUasHwC1bClQxRyaPCUOZ7a5C"
+})
+result = json.loads(connection.getresponse().read())
+
+#Loops through and adds each object's ID to array
+for i in range(0, len(result['results'])):
+    print ((result['results'])[i])['objectId']
+    idNumbers.append(((result['results'])[i])['objectId'])
+
+#Deletes every item one by one (Parse doesn't really do mass deletion)
+for i in idNumbers:
+    print '/1/classes/Clubs/' + i
+    connection.request('DELETE', '/1/classes/Clubs/' + i, '', {
+       "X-Parse-Application-Id": "1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw",
+       "X-Parse-REST-API-Key": "wL9fgRcbDT7UE6slUasHwC1bClQxRyaPCUOZ7a5C"
+    })
+    result = json.loads(connection.getresponse().read())
+    #print result
+    '''
+#Posts updated info to Parse, starting with an empty database
+
+connection.request('POST', '/1/classes/SchoolAlert', json.dumps(
+{
+    "Event": "2hr delay",
+    "willOccurOn": "April 2 2015"
+}), 
+{
+    "X-Parse-Application-Id": "1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw",
+    "X-Parse-REST-API-Key": "wL9fgRcbDT7UE6slUasHwC1bClQxRyaPCUOZ7a5C",
+    "Content-Type": "application/json"
+})
+result = json.loads(connection.getresponse().read())
+print result
