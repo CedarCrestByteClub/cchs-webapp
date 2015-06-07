3ea2b780c4271d272401303fd3cef09c3e363614
ljcrafty <ljcrafty@comcast.net>
2015-06-06 22:22:23 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 8ece5ff06104f8b18b77ab9a2f0c1f22ffd0f965, reversing
    changes made to 382359d4c3c0879218935980c539d7d683e52386.

--- www/php/load.php
+++ www/php/load.php
@@ -1,21 +1,12 @@
-<?php
-	//LEJ I started this and might not finish it...
-	//if network is available
-	try
-	{
-		//open files
-		$lunch = fopen("lunch.txt", "w") or die("Unable to open lunch file.");
-		$clubs = fopen("clubs.txt", "w") or die("Unable to open clubs file.");
-		$sports = fopen("sports.txt", "w") or die("Unable to open sports file.");
-		$news = fopen("news.txt", "w") or die("Unable to open news file.");
-		$notif = fopen("notif.txt", "w") or die("Unable to open notif file.");
-		
-		//okay, I dont know PHP. I'll admit that... future generations, please fix
-		Parse.initialize("1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw", "nNWuXfGI2ujbWBzxd5Om3F3OgFIIZRfvonOxfdEc");
-		fwrite($lunch, "did do");
-	}
-	catch(Exception $e) 
-	{
-		fwrite($lunch, "did not do");
-	}
-?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<?php
+			//if network is available
+			try
+			{
+				echo "ayo";
+			}
+		?>
+	</body>
+</html>
