0c84909bd254c0ef5792b27eb03e22adf5834134
ljcrafty <ljcrafty@comcast.net>
2015-08-04 15:46:30 -0400

    Looking for issues
    and fixing the sandwich cause it bugs me.

--- www/alertAdd.py
+++ www/alertAdd.py
@@ -9,29 +9,7 @@ connection = httplib.HTTPSConnection('api.parse.com', 443)
 connection.connect()
 idNumbers = []
 params = urllib.urlencode({"limit":1000})
-'''
-#Sends request and returns results in a list
-connection.request('GET', '/1/classes/SchoolAlert?%s' % params, '', {
-    "X-Parse-Application-Id": "1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw",
-    "X-Parse-REST-API-Key": "wL9fgRcbDT7UE6slUasHwC1bClQxRyaPCUOZ7a5C"
-})
-result = json.loads(connection.getresponse().read())
 
-#Loops through and adds each object's ID to array
-for i in range(0, len(result['results'])):
-    print ((result['results'])[i])['objectId']
-    idNumbers.append(((result['results'])[i])['objectId'])
-
-#Deletes every item one by one (Parse doesn't really do mass deletion)
-for i in idNumbers:
-    print '/1/classes/Clubs/' + i
-    connection.request('DELETE', '/1/classes/Clubs/' + i, '', {
-       "X-Parse-Application-Id": "1nbCZcm4WHUpYs0C89oTo231mhcpL2LRa5KfsYtw",
-       "X-Parse-REST-API-Key": "wL9fgRcbDT7UE6slUasHwC1bClQxRyaPCUOZ7a5C"
-    })
-    result = json.loads(connection.getresponse().read())
-    #print result
-    '''
 #Posts updated info to Parse, starting with an empty database
 
 connection.request('POST', '/1/classes/SchoolAlert', json.dumps(
